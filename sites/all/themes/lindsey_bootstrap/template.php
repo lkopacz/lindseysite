602b3e8ce9bf46117922f0f545035bda9edcac51
Rich Allen <lindsey.kopacz@gmail.com>
2015-06-15 19:49:57 -0400

    updated blog page and updated styling, made navbar fixed

--- sites/all/themes/lindsey_bootstrap/template.php
+++ sites/all/themes/lindsey_bootstrap/template.php
@@ -11,3 +11,7 @@ function lindsey_bootstrap_preprocess_html(&$variables) {
   drupal_add_js('https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', 'external');
   drupal_add_js(drupal_get_path('theme', 'lindsey_bootstrap') . '/js/script.js');
 }
+
+function lindsey_bootstrap_preprocess_page(&$variables) {
+  $variables['navbar_classes_array'][] = 'navbar-fixed-top';
+}
